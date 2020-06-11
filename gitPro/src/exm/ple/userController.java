5f4973b28787ac866bc9b0853ee6f63b7110d1ff
huangjinjin22 <2669406081@qq.com>
2020-06-11 09:05:23 +0800

    提交

---
+++ gitPro/src/exm/ple/userController.java
@@ -0,0 +1,11 @@
+package exm.ple;
+
+public class userController {
+
+	public static void main(String[] args) {
+		user u=new user("¾¨Óã", 001, 18, "ÄÏÄşÑ§Ôº");
+		// TODO Auto-generated method stub
+
+	}
+
+}
